c2b6d46318d02ea6119118ade650d622fc15fbf7
Akshay-007 <akshayagopalece@gmail.com>
2017-04-24 15:05:07 +0530

    Project that uses the jar file created in TempConv

---
+++ TempConversionUser/src/main/java/com/user/TempConversionUser/App.java
@@ -0,0 +1,14 @@
+package com.user.TempConversionUser;
+import com.atlantis.tempConv.*;
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+        System.out.println(TempConversion.fahrenheitToCelcius(90));
+    }
+}
